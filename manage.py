254ed096bbfc16de771b930dc6240ff21e379bc1
pppppython <2223601321@qq.com>
2020-02-15 20:57:59 +0800

    first commit

---
+++ manage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from bluelog import create_app
+
+
+app = create_app()
+
+
+
+
+if __name__ == '__main__':
+    #app.run(host='0.0.0.0', port=5001)
+    app.run(host="127.0.0.1", debug=True)
